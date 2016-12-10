866b9b1f5718dbcc9e2ead5ee70c31cad27cf2a8
Yehonatan Sayag <sayag11@gmail.com>
2016-12-10 19:22:36 +0200

    exp exp exp expexp exp exp expexp exp exp expexp exp exp expexp exp exp expexp exp exp exp

--- EX3/LINUX_COMMAND_LINE/FOLDER_3_src/AST/AST_STMT_RETURN.java
+++ EX3/LINUX_COMMAND_LINE/FOLDER_3_src/AST/AST_STMT_RETURN.java
@@ -2,9 +2,12 @@ package AST;
 
 public class AST_STMT_RETURN extends AST_STMT
 {
+	public AST_EXP exp;
 
 	/*******************/
 	/*  CONSTRUCTOR(S) */
 	/*******************/
-	public AST_STMT_RETURN(){}
+	public AST_STMT_RETURN(exp){
+		this.exp = exp;
+	}
 }
